d1695d982a30b927d42b2b3db7fcae9003116e8c
isalinda <105506514+isacampos28@users.noreply.github.com>
2024-10-23 08:51:53 -0300

    Add files via upload

---
+++ isabella 3b/script.js
@@ -0,0 +1,21 @@
+var ctx = document.getElementById("myChart").getContext("2dc")
+var myChart = new Chart(ctx,{
+type: "pie",
+Date: {
+    labels: [
+"instagram",
+"whatsapp",
+"facebook",
+"WeChat",
+"tiktok",
+"DouYin",
+"twitter",
+"telegram",
+"fb messenger",
+"LINE"
+]
+
+
+}
+}
+) 
